e9defbb92186664707845952cc1ca5ecc026344e
mdnihal5 <mdnihal.private1@gmail.com>
2025-02-15 08:07:40 +0530

    fix cors

--- api/app.js
+++ api/app.js
@@ -18,7 +18,7 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://wellvisit.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   }),
